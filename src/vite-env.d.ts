c60f678e25188c7f1b12455a3ba0f78bcd7fa8b9
吴再发 <wuzaifa@wuzaifadeMacBook-Pro.local>
2023-03-26 00:22:43 +0800

    feat: 🚀 文件结构创建
    基本搭建-文件结构

---
+++ src/vite-env.d.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: Gauche楽
+ * @Date: 2023-03-26 00:16:55
+ * @LastEditors: Gauche楽
+ * @LastEditTime: 2023-03-26 00:17:00
+ * @FilePath: /vite-project/src/vite-env.d.ts
+ */
+/// <reference types="vite/client" />
