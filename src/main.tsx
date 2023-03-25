bd1a778cc264587df871eaadd16ce1c5d848b45e
吴再发 <wuzaifa@wuzaifadeMacBook-Pro.local>
2023-03-26 01:37:51 +0800

    feat: 🚀 引入normalize.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@
  * @Author: Gauche楽
  * @Date: 2023-03-24 15:09:23
  * @LastEditors: Gauche楽
- * @LastEditTime: 2023-03-25 02:38:48
+ * @LastEditTime: 2023-03-26 01:36:04
  * @FilePath: /vite-project/src/main.tsx
  */
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "normalize.css";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
