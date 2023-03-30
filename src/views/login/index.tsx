da98ac8e413e5716396fd6b91cea911f95262be7
吴再发 <wuzaifa@wuzaifadeMacBook-Pro.local>
2023-03-30 22:49:17 +0800

    style: 🎨 修改登录页样式

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -7,7 +7,7 @@ import LoginForm from "./components/LoginFrom";
 const Login = () => {
 	return (
 		<div className="login-container">
-			<div className="login-content">
+			<div className="login-box">
 				<div className="login-left">
 					<img src={loginLeft} alt="login" />
 				</div>
