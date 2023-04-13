b2b48669b54ce9c608b529e108adefb3fa2613a0
吴再发 <wuzaifa@wuzaifadeMacBook-Pro.local>
2023-04-13 23:06:05 +0800

    feat: 🚀 新增按钮权限hooks

---
+++ src/hooks/useAuthButtons.ts
@@ -0,0 +1,24 @@
+/*
+ * @Author: Gauche楽
+ * @Date: 2023-04-13 22:45:00
+ * @LastEditors: Gauche楽
+ * @LastEditTime: 2023-04-13 23:00:33
+ * @FilePath: /vite-project/src/hooks/useAuthButtons.ts
+ */
+/**
+ * @description 页面按钮权限
+ */
+
+import { store } from "@/redux";
+import { routerArray } from "@/routers";
+import { searchRoute } from "@/utils/util";
+import { useLocation } from "react-router-dom";
+
+export const useAuthButtons = () => {
+	const { pathname } = useLocation();
+	const route = searchRoute(pathname, routerArray);
+
+	return {
+		BUTTONS: store.getState().auth.authButtons[route.meta!.key!] || {}
+	};
+};
--- src/views/proTable/useHooks/index.tsx
+++ src/views/proTable/useHooks/index.tsx
@@ -2,7 +2,7 @@
  * @Author: Gauche楽
  * @Date: 2023-03-30 00:36:06
  * @LastEditors: Gauche楽
- * @LastEditTime: 2023-04-12 23:46:11
+ * @LastEditTime: 2023-04-13 23:05:36
  * @FilePath: /vite-project/src/views/proTable/useHooks/index.tsx
  */
 import { DatePicker, Table } from "antd";
@@ -11,11 +11,13 @@ import type { ReactNode } from "react";
 
 import "./index.less";
 import { connect } from "react-redux";
+import { useAuthButtons } from "@/hooks/useAuthButtons";
 
 interface IProps {
 	children?: ReactNode;
 }
 const UseHooks: React.FC<IProps> = (props: any) => {
+	const { BUTTONS } = useAuthButtons();
 	const { RangePicker } = DatePicker;
 	useEffect(() => {
 		console.log(props.authButtons, "authButtons");
@@ -55,7 +57,8 @@ const UseHooks: React.FC<IProps> = (props: any) => {
 	];
 	return (
 		<>
-			<RangePicker /> <Table dataSource={dataSource} columns={columns} />
+			{BUTTONS.add ? <RangePicker /> : null}
+			<Table dataSource={dataSource} columns={columns} />
 		</>
 	);
 };
